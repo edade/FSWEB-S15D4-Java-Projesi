a665871291c804743a19247f1ca214e71f169ea6
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-02-07 16:29:14 +0300

    Stack Challenge done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,6 +12,26 @@ public class Main {
     }
 
     public static boolean checkForPalindrome(String text) {
+        System.out.println("text: "+ text );
+        text = text.replaceAll("[.,'!?\\-\\s]","");
+        Stack<Character> stack = new Stack<>();
+        Stack<Character> stackTemp = new Stack<>();
+        Stack<Character>  stackReversed = new Stack<>();
+        char[] chars = text.toCharArray();
+        for(char c : chars){
+            stack.push(c);
+            stackTemp.push(c);
+        }
+        for (char c : chars){
+            Character charTemp = stackTemp.pop();
+            stackReversed.push((Character)charTemp);
+        }
+        System.out.println(stackReversed);
+        System.out.println(stack);
+
+        if(stack.equals(stackReversed)){
+           return true;
+        }
         return false;
     }
 }
